7fef375fef22b1eb78d51105dcf2c7173450f3ae
unknown <ejbiasi@hotmail.com>
2024-06-26 19:56:22 -0300

    front only - form analytics owner 100%

--- src/screens/FormAnalytics/FormAnalyticsOwner.tsx
+++ src/screens/FormAnalytics/FormAnalyticsOwner.tsx
@@ -11,120 +11,32 @@ import {
 } from "./styles/FormAnalyticsOwnerStyles";
 import logo from "../../assets/logos/logo.svg";
 import { RocketLaunch } from "phosphor-react";
+import { QuestionaryData } from "../../utils/FormAnalytics/Questionary";
 
-const website_questions = [
-  {
-    question: "What is your website's monthly traffic?",
-    input_type: "multiple_choice",
-    options: ["0 - 1,000", "1,001 - 5,000", "5,001 - 10,000", "10,001+"],
-  },
-  {
-    question: "What are the key performance metrics for your website?",
-    input_type: "multiple_choice",
-    options: ["Bounce rate", "Average session duration", "Conversion rate"],
-  },
-];
+interface Question {
+  question: string;
+  answer: string;
+  input_type: "text" | "textarea" | "multiple_choice";
+  loading: boolean;
+  options?: string[];
+}
 
-const social_media_presence_questions = [
-  {
-    question: "Which social media platforms do you actively use?",
-    input_type: "multiple_choice",
-    options: ["Facebook", "LinkedIn", "Twitter", "Instagram"],
-  },
-  {
-    question: "What are your engagement metrics on these platforms?",
-    input_type: "text",
-  },
-];
-
-const seo_performance_questions = [
-  {
-    question: "What are your main target keywords?",
-    input_type: "text",
-  },
-  {
-    question: "How is your website currently ranking for these keywords?",
-    input_type: "text",
-  },
-];
-
-const paid_advertising_questions = [
-  {
-    question:
-      "Do you run any paid advertising campaigns? If yes, on which platforms?",
-    input_type: "multiple_choice",
-    options: ["Google Ads", "Facebook Ads", "LinkedIn Ads"],
-  },
-  {
-    question: "What is the performance of these campaigns?",
-    input_type: "text",
-  },
-];
-
-const budget_questions = [
-  {
-    question:
-      "What is your current budget for digital marketing and growth hacking initiatives?",
-    input_type: "multiple_choice",
-    options: [
-      "Less than $5,000",
-      "$5,001 - $10,000",
-      "$10,001 - $20,000",
-      "Over $20,000",
-    ],
-  },
-];
-
-const tools_and_technologies_questions = [
-  {
-    question:
-      "What tools and platforms do you use for marketing and analytics?",
-    input_type: "multiple_choice",
-    options: ["Google Analytics", "HubSpot", "Salesforce"],
-  },
-];
-
-const team_and_skills_questions = [
-  {
-    question: "How is your marketing team structured?",
-    input_type: "text",
-  },
-  {
-    question: "What are the key skills and expertise within your team?",
-    input_type: "multiple_choice",
-    options: ["SEO expertise", "Content creation", "Social media management"],
-  },
-];
+interface QuestionarySection {
+  section: string;
+  questions: Question[];
+}
 
 const FormAnalyticsOwner: React.FC = () => {
-  const all_questions = [
-    { title: "Website Analytics", questions: website_questions },
-    {
-      title: "Social Media Presence",
-      questions: social_media_presence_questions,
-    },
-    { title: "SEO Performance", questions: seo_performance_questions },
-    { title: "Paid Advertising", questions: paid_advertising_questions },
-    { title: "Budget", questions: budget_questions },
-    {
-      title: "Tools and Technologies",
-      questions: tools_and_technologies_questions,
-    },
-    { title: "Team and Skills", questions: team_and_skills_questions },
-  ];
+  const [formData, setFormData] =
+    useState<QuestionarySection[]>(QuestionaryData);
 
-  const totalQuestions = all_questions.reduce(
-    (acc, section) => acc + section.questions.length,
-    0
-  );
-
-  const [formData, setFormData] = useState<string[]>(
-    Array(totalQuestions).fill("")
-  );
-
-  const handleInputChange = (index: number, value: string) => {
+  const handleInputChange = (
+    sectionIndex: number,
+    questionIndex: number,
+    value: string
+  ) => {
     const newFormData = [...formData];
-    newFormData[index] = value;
+    newFormData[sectionIndex].questions[questionIndex].answer = value;
     setFormData(newFormData);
   };
 
@@ -133,15 +45,12 @@ const FormAnalyticsOwner: React.FC = () => {
     console.log("Form data submitted:", formData);
   };
 
-  let questionIndex = 0;
-
   return (
     <Container>
       <Main>
         <Logo>
           <img src={logo} alt="Logo synesis" />
         </Logo>
-
         <Title>
           <h1>Not long!</h1>
           <p>
@@ -149,61 +58,60 @@ const FormAnalyticsOwner: React.FC = () => {
             generated at the end.
           </p>
         </Title>
-
         <Form onSubmit={handleSubmit}>
-          {all_questions.map((section, sectionIndex) => (
+          {formData.map((section, sectionIndex) => (
             <div key={sectionIndex}>
-              <h3>{section.title}</h3>
-              {section.questions.map((question, questionIdx) => {
-                return (
-                  <>
-                    <Question key={questionIdx}>
-                      <label>
-                        {`${questionIndex + 1}. ${question.question}`}
-                      </label>
-                      {question.input_type === "text" && (
-                        <input
-                          type="text"
-                          value={formData[questionIndex]}
-                          onChange={(e) =>
-                            handleInputChange(questionIndex, e.target.value)
+              <h3>{section.section}</h3>
+              {section.questions.map((question, questionIdx) => (
+                <Question key={questionIdx}>
+                  <label>{`${question.question}`}</label>
+                  {question.input_type === "text" && (
+                    <input
+                      type="text"
+                      value={question.answer}
+                      onChange={(e) =>
+                        handleInputChange(
+                          sectionIndex,
+                          questionIdx,
+                          e.target.value
+                        )
+                      }
+                      placeholder="Type here..."
+                    />
+                  )}
+                  {question.input_type === "textarea" && (
+                    <textarea
+                      value={question.answer}
+                      onChange={(e) =>
+                        handleInputChange(
+                          sectionIndex,
+                          questionIdx,
+                          e.target.value
+                        )
+                      }
+                      placeholder="Type here..."
+                    />
+                  )}
+                  {question.input_type === "multiple_choice" && (
+                    <div className="buttonGroup">
+                      {question.options?.map((option, optionIndex) => (
+                        <Button
+                          type="button"
+                          key={optionIndex}
+                          onClick={() =>
+                            handleInputChange(sectionIndex, questionIdx, option)
                           }
-                          placeholder="Type here..."
-                        />
-                      )}
-                      {question.input_type === "textarea" && (
-                        <textarea
-                          value={formData[questionIndex]}
-                          onChange={(e) =>
-                            handleInputChange(questionIndex, e.target.value)
-                          }
-                          placeholder="Type here..."
-                        />
-                      )}
-                      {question.input_type === "multiple_choice" && (
-                        <div className="buttonGroup">
-                          {question.options?.map((option, optionIndex) => (
-                            <Button
-                              type="button"
-                              key={optionIndex}
-                              onClick={() =>
-                                handleInputChange(questionIndex, option)
-                              }
-                              selected={formData[questionIndex] === option}
-                            >
-                              {option}
-                            </Button>
-                          ))}
-                        </div>
-                      )}
-                    </Question>
-                    {questionIndex++}
-                  </>
-                );
-              })}
+                          selected={question.answer === option}
+                        >
+                          {option}
+                        </Button>
+                      ))}
+                    </div>
+                  )}
+                </Question>
+              ))}
             </div>
           ))}
-
           <Action>
             <button type="submit">
               Submit <RocketLaunch size={20} className="icon__rocket" />
---
+++ src/utils/FormAnalytics/Questionary.tsx
@@ -0,0 +1,142 @@
+interface Question {
+  question: string;
+  answer: string;
+  input_type: "text" | "textarea" | "multiple_choice";
+  loading: boolean;
+  options?: string[];
+}
+
+interface QuestionarySection {
+  section: string;
+  questions: Question[];
+}
+
+export const QuestionaryData: QuestionarySection[] = [
+  {
+    section: "Website Analytics",
+    questions: [
+      {
+        question: "1. What is your website's monthly traffic?",
+        answer: "",
+        input_type: "multiple_choice",
+        options: ["0 - 1,000", "1,001 - 5,000", " 5,001 - 10,000", "10,001+"],
+        loading: false,
+      },
+      {
+        question: "2. What are the key performance metrics for your website?",
+        answer: "",
+        input_type: "multiple_choice",
+        options: ["Bounce rate", "Average session duration", "Conversion rate"],
+        loading: false,
+      },
+    ],
+  },
+  {
+    section: "Social Media Presence",
+    questions: [
+      {
+        question: "3. Which social media platforms do you actively use?",
+        answer: "",
+        input_type: "multiple_choice",
+        options: ["Facebook", "LinkedIn", "Twitter", "Instagram"],
+        loading: false,
+      },
+      {
+        question: "4. What are your engagement metrics on these platforms?",
+        answer: "",
+        input_type: "textarea",
+        loading: false,
+      },
+    ],
+  },
+  {
+    section: "SEO Performance",
+    questions: [
+      {
+        question: "5. What are your main target keywords?",
+        answer: "",
+        input_type: "textarea",
+        loading: false,
+      },
+      {
+        question:
+          "6. How is your website currently ranking for these keywords?",
+        answer: "",
+        input_type: "textarea",
+        loading: false,
+      },
+    ],
+  },
+  {
+    section: "Paid Advertising",
+    questions: [
+      {
+        question:
+          "7. Do you run any paid advertising campaigns? If yes, on which platforms?",
+        answer: "",
+        input_type: "multiple_choice",
+        options: ["Google Ads", "Facebook Ads", "LinkedIn Ads"],
+        loading: false,
+      },
+      {
+        question: "8. What is the performance of these campaigns?",
+        answer: "",
+        input_type: "textarea",
+        loading: false,
+      },
+    ],
+  },
+  {
+    section: "Budget",
+    questions: [
+      {
+        question:
+          "9.  What is your current budget for digital marketing and growth hacking initiatives?",
+        answer: "",
+        input_type: "multiple_choice",
+        options: [
+          "Less than $5,000",
+          "$5,001 - $10,000",
+          "$10,001 - $20,000",
+          "Over $20,000",
+        ],
+        loading: false,
+      },
+    ],
+  },
+  {
+    section: "Tools and Technologies",
+    questions: [
+      {
+        question:
+          "10. What tools and platforms do you use for marketing and analytics?",
+        answer: "",
+        input_type: "multiple_choice",
+        options: ["Google Analytics", "HubSpot", "Salesforce"],
+        loading: false,
+      },
+    ],
+  },
+  {
+    section: "Team and Skills",
+    questions: [
+      {
+        question: "11. How is your marketing team structured?",
+        answer: "",
+        input_type: "textarea",
+        loading: false,
+      },
+      {
+        question: "12. What are the key skills and expertise within your team?",
+        answer: "",
+        input_type: "multiple_choice",
+        options: [
+          "SEO expertise",
+          "Content creation",
+          "Social media management",
+        ],
+        loading: false,
+      },
+    ],
+  },
+];
